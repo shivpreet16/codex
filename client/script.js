8b2050daf67771a107740a7d68393cf932c7425b
shivpreet <shivpreet16@gmail.com>
2023-01-04 13:17:06 +0530

    updated-server-url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e)=>{
 
     loader(messagediv)
 
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://codex-lxuk.onrender.com',{
         method: 'POST',
         headers:{
             'Content-Type':'application/json'
